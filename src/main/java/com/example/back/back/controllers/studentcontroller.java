fca6f5c0bb9d15e3706442a9c933686c983badd9
kn-eya <eyakousseni@gmail.com>
2024-03-07 18:11:03 +0100

    Add changes to StudentController.java

--- src/main/java/com/example/back/back/controllers/studentcontroller.java
+++ src/main/java/com/example/back/back/controllers/studentcontroller.java
@@ -2,18 +2,26 @@ package com.example.back.back.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.util.ArrayList;
+import java.util.List;
 @RestController
 @RequestMapping
 
 public class studentcontroller {
     @Autowired
     
-    @GetMapping("/student")
-    public String getAllStudent(){
-        return "";
-    }
+
+   @PostMapping("/List")
+    public List<String> findallStudents(){
+        return List.of(
+             "Eya",
+             "Hello"
+        );
+
+    };
 
 }
+
